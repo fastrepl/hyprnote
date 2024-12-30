d231761d4b2e1e641251155fa2bc053a0ba451b8
yujonglee <yujonglee.dev@gmail.com>
2024-12-30 17:01:43 +0900

    add tanstack query

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,11 +1,21 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { routeTree } from "./routeTree.gen";
 import "./styles/global.css";
 
-const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: "intent",
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -14,11 +24,12 @@ declare module "@tanstack/react-router" {
 }
 
 const rootElement = document.getElementById("root")!;
+
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </StrictMode>,
+    </QueryClientProvider>,
   );
 }

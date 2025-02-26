e098611d01924f48c7b7215bcd17c2e10359b416
yujonglee <yujonglee.dev@gmail.com>
2025-02-26 19:26:46 +0900

    fix auth callback handling

--- plugins/auth/src/ext.rs
+++ plugins/auth/src/ext.rs
@@ -34,8 +34,10 @@ impl<R: tauri::Runtime, T: tauri::Manager<R>> AuthPluginExt<R> for T {
         let vault = self.state::<Vault>().inner().clone();
 
         let response = env
-            .render_str(CALLBACK_TEMPLATE_KEY, &serde_json::Map::new())
-            .unwrap()
+            .get_template(CALLBACK_TEMPLATE_KEY)
+            .map_err(|e| e.to_string())?
+            .render(&serde_json::Map::new())
+            .map_err(|e| e.to_string())?
             .into();
 
         let port = tauri_plugin_oauth::start_with_config(
@@ -43,14 +45,23 @@ impl<R: tauri::Runtime, T: tauri::Manager<R>> AuthPluginExt<R> for T {
                 ports: None,
                 response: Some(response),
             },
-            move |url| match serde_qs::from_str::<ResponseParams>(&url) {
-                Ok(params) => {
-                    tracing::info!(params = ?params, "auth_callback");
-                    vault.set(Key::RemoteServer, params.token).unwrap();
-                    vault.set(Key::UserId, params.user_id).unwrap();
-                }
-                Err(err) => {
-                    tracing::error!("failed_to_parse_callback_params: {}", err);
+            move |u| {
+                let search = url::Url::parse(&u)
+                    .map_err(|e| e.to_string())
+                    .and_then(|url| {
+                        serde_qs::from_str::<ResponseParams>(url.query().unwrap_or(""))
+                            .map_err(|e| e.to_string())
+                    });
+
+                match search {
+                    Ok(params) => {
+                        tracing::info!(params = ?params, "auth_callback");
+                        vault.set(Key::RemoteServer, params.token).unwrap();
+                        vault.set(Key::UserId, params.user_id).unwrap();
+                    }
+                    Err(err) => {
+                        tracing::error!(error = ?err, url = ?u, "failed_to_parse_callback_params");
+                    }
                 }
             },
         )
@@ -71,5 +82,19 @@ impl<R: tauri::Runtime, T: tauri::Manager<R>> AuthPluginExt<R> for T {
     fn get_from_vault(&self, key: Key) -> Result<Option<String>, String> {
         let vault = self.state::<Vault>();
         vault.get(key).map_err(|err| err.to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_qs() {
+        assert!(serde_qs::from_str::<RequestParams>("c=123&f=456&p=789").is_ok());
+        assert!(serde_qs::from_str::<RequestParams>(
+            "http://localhost:8080/auth/connect?c=123&f=456&p=789"
+        )
+        .is_err())
     }
 }

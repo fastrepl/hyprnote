5e8e9c8318277e11c27374128bfcb0b25a3ee65e
John Jeong <jeeheontransformers@gmail.com>
2025-03-02 20:05:15 +0900

    home under construction

--- apps/desktop/src/routes/app.home.tsx
+++ apps/desktop/src/routes/app.home.tsx
@@ -1,9 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import MyTasks from "@/components/home/my-tasks";
+import RecentNotes from "@/components/home/recent-notes";
+import WorkspaceCalendar from "@/components/home/workspace-calendar";
+
 export const Route = createFileRoute("/app/home")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/app/home"!</div>;
+  return (
+    <div className="max-w-5xl mx-auto p-8 overflow-y-auto h-full">
+      <RecentNotes />
+      <WorkspaceCalendar />
+      <MyTasks />
+    </div>
+  );
 }

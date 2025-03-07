f8fca087992cc4f0eebddb0bbbe3562751409be9
yujonglee <yujonglee.dev@gmail.com>
2025-03-07 13:22:48 +0900

    config

--- apps/mobile/vite.config.ts
+++ apps/mobile/vite.config.ts
@@ -1,32 +1,35 @@
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
+  ...tauri,
+}));
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+// https://v2.tauri.app/start/frontend/vite/#update-vite-configuration
+const tauri: UserConfig = {
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
+    port: 1430,
     strictPort: true,
     host: host || false,
     hmr: host
       ? {
           protocol: "ws",
           host,
-          port: 1421,
+          port: 1431,
         }
       : undefined,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
-}));
+  envPrefix: ["VITE_", "TAURI_ENV_*"],
+  build: {
+    outDir: "./dist",
+    chunkSizeWarningLimit: 500 * 10,
+  },
+};

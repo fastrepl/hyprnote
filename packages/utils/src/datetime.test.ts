f4f3814017e4983ce4d6a4f781b5d2761525b059
yujonglee <yujonglee.dev@gmail.com>
2025-03-13 17:39:33 +0900

    wip

--- packages/utils/src/datetime.test.ts
+++ packages/utils/src/datetime.test.ts
@@ -1,9 +1,11 @@
 import { describe, expect, it } from "vitest";
-
-import { formatDate } from "./datetime";
+import { differenceInBusinessDays } from "./datetime";
 
 describe("datetime", () => {
   it("should be a function", () => {
-    expect(formatDate(new Date(), "yyyy-MM-dd")).toBe("2025-03-13");
+    const dateString = "2025-03-13T07:05:07Z";
+    const now = new Date();
+
+    expect(differenceInBusinessDays(now, new Date(dateString), "Asia/Seoul")).toBe(0);
   });
 });
--- packages/utils/src/datetime.ts
+++ packages/utils/src/datetime.ts
@@ -1,5 +1,82 @@
-import { format } from "date-fns";
+import * as FNS_TZ from "@date-fns/tz";
+import * as FNS from "date-fns";
+import * as FNS_LOCALE from "date-fns/locale";
 
-export const formatDate = (date: Date, formatString: string) => {
-  return format(date, formatString);
+export const timezone = () => {
+  if (typeof window === "undefined") {
+    throw new Error("timezone is only available on browser");
+  }
+
+  return Intl.DateTimeFormat().resolvedOptions().timeZone;
+};
+
+export const differenceInBusinessDays = (
+  d1: Parameters<typeof FNS.differenceInBusinessDays>[0],
+  d2: Parameters<typeof FNS.differenceInBusinessDays>[1],
+  t?: string,
+) => {
+  return FNS.differenceInBusinessDays(d1, d2, { in: FNS_TZ.tz(t || timezone()) });
+};
+
+// TODO: use FNS_LOCALE
+export const renderDaysDiff = (diff: number, t?: string) => {
+  const tz = t || timezone();
+
+  if (diff === 0) {
+    if (tz === "Asia/Seoul") {
+      return "오늘";
+    }
+
+    return "Today";
+  }
+
+  if (diff === 1) {
+    if (tz === "Asia/Seoul") {
+      return "어제";
+    }
+
+    return "Yesterday";
+  }
+
+  if (diff === 2) {
+    if (tz === "Asia/Seoul") {
+      return "그저께";
+    }
+
+    return "2 days ago";
+  }
+
+  if (diff < 7) {
+    if (tz === "Asia/Seoul") {
+      return `${diff} 일 전`;
+    }
+
+    return `${diff} days ago`;
+  }
+
+  if (diff < 14) {
+    if (tz === "Asia/Seoul") {
+      return "저번 주";
+    }
+
+    return "Last week";
+  }
+
+  if (diff < 21) {
+    if (tz === "Asia/Seoul") {
+      return "2주 전";
+    }
+
+    return "2 weeks ago";
+  }
+
+  if (diff < 30) {
+    if (tz === "Asia/Seoul") {
+      return "3주 전";
+    }
+
+    return "3 weeks ago";
+  }
+
+  return `${diff} days ago`;
 };

c57067f155c09b09c08ae119fd30fc0077e27242
yujonglee <yujonglee.dev@gmail.com>
2025-03-14 09:02:38 +0900

    fix import

--- apps/desktop/src/routes/app.note.$id.sub.tsx
+++ apps/desktop/src/routes/app.note.$id.sub.tsx
@@ -2,7 +2,7 @@ import { QueryClient } from "@tanstack/react-query";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { useEffect } from "react";
 
-import EditorArea from "@/components/note/editor";
+import EditorArea from "@/components/note/editor-area";
 import { useSession } from "@/contexts/session";
 import { commands as dbCommands, type Session } from "@hypr/plugin-db";
 

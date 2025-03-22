1982b13a531f0f1e394e5fb236c9fe54152c0d85
John Jeong <jeeheontransformers@gmail.com>
2025-03-23 01:01:36 +0900

    removed unused icon

--- apps/desktop/src/components/note/header/listen-button.tsx
+++ apps/desktop/src/components/note/header/listen-button.tsx
@@ -1,4 +1,4 @@
-import { Ear, EarOff, Mic, MicIcon, SpeakerIcon } from "lucide-react";
+import { Ear, EarOff, MicIcon, SpeakerIcon } from "lucide-react";
 import { useState } from "react";
 
 import SoundIndicator from "@/components/sound-indicator";

32fe5ce768d0c16cdf8c8178d65d51c262f312ad
John Jeong <jeeheontransformers@gmail.com>
2025-03-30 20:34:47 +0900

    refactored

---
+++ apps/desktop/src/components/editor-area/enhance-button/index.tsx
@@ -0,0 +1,45 @@
+import { useOngoingSession } from "@/contexts";
+import { Session } from "@hypr/plugin-db";
+import { EnhanceControls } from "./enhance-controls";
+import { EnhanceOnlyButton } from "./enhance-only-button";
+
+interface EnhanceButtonProps {
+  handleClick: () => void;
+  session: Session;
+  showRaw: boolean;
+  setShowRaw: (showRaw: boolean) => void;
+  enhanceStatus: "error" | "idle" | "pending" | "success";
+}
+
+export function EnhanceButton({
+  handleClick,
+  session,
+  showRaw,
+  setShowRaw,
+  enhanceStatus,
+}: EnhanceButtonProps) {
+  const ongoingSessionStore = useOngoingSession((s) => ({
+    status: s.status,
+    timeline: s.timeline,
+  }));
+
+  if (!ongoingSessionStore.timeline?.items.length) {
+    return null;
+  }
+
+  return (session.enhanced_memo_html || enhanceStatus === "pending")
+    ? (
+      <EnhanceControls
+        showRaw={showRaw}
+        setShowRaw={setShowRaw}
+        enhanceStatus={enhanceStatus}
+        handleRunEnhance={handleClick}
+      />
+    )
+    : (
+      <EnhanceOnlyButton
+        handleRunEnhance={handleClick}
+        enhanceStatus={enhanceStatus}
+      />
+    );
+}

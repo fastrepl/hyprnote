4b56dc786b75eab61cee517ae02d9a21f266b6e9
yujonglee <yujonglee.dev@gmail.com>
2025-04-07 15:46:51 +0900

    update prompt

--- crates/whisper/src/local/model.rs
+++ crates/whisper/src/local/model.rs
@@ -6,6 +6,7 @@ use whisper_rs::{
 
 pub struct Whisper {
     state: WhisperState,
+    prompt: String,
 }
 
 impl Whisper {
@@ -29,27 +30,30 @@ impl Whisper {
         let ctx = WhisperContext::new_with_params(model_path.as_ref(), context_param).unwrap();
         let state = ctx.create_state().unwrap();
 
-        Self { state }
+        Self {
+            state,
+            prompt: "".to_string(),
+        }
     }
 
     pub fn transcribe(&mut self, audio: &[f32]) -> Vec<Segment> {
         let params = {
             let mut p = FullParams::new(SamplingStrategy::Greedy { best_of: 1 });
-            p.set_n_threads(1);
             p.set_translate(false);
             p.set_language(Some("en"));
+            p.set_initial_prompt(self.prompt.as_str());
+
+            p.set_n_threads(1);
+            p.set_token_timestamps(true);
+            p.set_split_on_word(true);
+            p.set_single_segment(true);
+            p.set_suppress_blank(true);
+            p.set_suppress_nst(true);
 
             p.set_print_special(false);
             p.set_print_progress(false);
             p.set_print_realtime(false);
             p.set_print_timestamps(false);
-
-            p.set_token_timestamps(true);
-            p.set_initial_prompt("");
-            p.set_split_on_word(true);
-            p.set_single_segment(false);
-            p.set_suppress_blank(true);
-            p.set_suppress_nst(true);
             p
         };
 
@@ -63,6 +67,7 @@ impl Whisper {
                 self.state.full_get_segment_t0(i).unwrap(),
                 self.state.full_get_segment_t1(i).unwrap(),
             );
+
             segments.push(Segment {
                 text,
                 start: start as f32 / 1000.0,
@@ -71,6 +76,13 @@ impl Whisper {
                 confidence: 0.99,
             });
         }
+
+        self.prompt = segments
+            .iter()
+            .map(|s| s.text())
+            .collect::<Vec<&str>>()
+            .join(" ");
+
         segments
     }
 }
@@ -110,7 +122,6 @@ mod tests {
     use super::*;
     use futures_util::StreamExt;
 
-    // https://github.com/utilityai/llama-cpp-rs/issues/484
     #[test]
     fn test_whisper() {
         let mut whisper = Whisper::new(concat!(env!("CARGO_MANIFEST_DIR"), "/model.bin"));

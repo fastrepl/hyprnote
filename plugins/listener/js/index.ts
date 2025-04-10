d5a20b955227bb1f31e5952aeff6a177a9d2f478
yujonglee <yujonglee.dev@gmail.com>
2025-04-10 17:49:08 +0900

    add const helpers

--- plugins/listener/js/index.ts
+++ plugins/listener/js/index.ts
@@ -1,1 +1,5 @@
 export * from "./bindings.gen";
+
+export const INACTIVE = "__LISTENER_TODO__inactive";
+export const RUNNING_ACTIVE = "__LISTENER_TODO__running_active";
+export const RUNNING_PAUSED = "__LISTENER_TODO__running_paused";

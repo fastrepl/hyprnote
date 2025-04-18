039b2762ca7a6d06841090fcc558c9115a215c65
yujonglee <yujonglee.dev@gmail.com>
2025-04-18 10:23:42 +0900

    ci pass?

--- crates/notification2/src/lib.rs
+++ crates/notification2/src/lib.rs
@@ -1,1 +1,11 @@
-pub use wezterm::*;
+pub use wezterm::{show, ToastNotification as Notification};
+
+#[cfg(target_os = "macos")]
+pub fn initialize() {
+    wezterm::macos_initialize();
+}
+
+#[cfg(not(target_os = "macos"))]
+pub fn initialize() {
+    // do nothing
+}

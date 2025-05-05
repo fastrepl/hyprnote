4f7f8da67aab242512f1e94218629bf87c92bc28
yujonglee <yujonglee.dev@gmail.com>
2025-05-04 22:53:32 -0700

    fix windows build

--- apps/desktop/src-tauri/src/ext.rs
+++ apps/desktop/src-tauri/src/ext.rs
@@ -139,6 +139,7 @@ impl<R: tauri::Runtime, T: tauri::Manager<R>> AppExt<R> for T {
                 hypr_db_user::init::seed(user_db, &user_id).await.unwrap();
             }
 
+            #[cfg(target_os = "macos")]
             {
                 use tauri_plugin_apple_calendar::AppleCalendarPluginExt;
                 self.start_worker(&user_id).await?;

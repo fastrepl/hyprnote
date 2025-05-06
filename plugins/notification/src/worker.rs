992378f7452b170aa10f342555a5d8127b1610b4
yujonglee <yujonglee.dev@gmail.com>
2025-05-05 21:19:08 -0700

    worker implementation

--- plugins/notification/src/worker.rs
+++ plugins/notification/src/worker.rs
@@ -1,5 +1,6 @@
 use apalis::prelude::{Data, Error, WorkerBuilder, WorkerFactoryFn};
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
+use hypr_db_user::{ListEventFilter, ListEventFilterCommon, ListEventFilterSpecific};
 
 #[allow(unused)]
 #[derive(Default, Debug, Clone)]
@@ -21,6 +22,31 @@ const EVENT_NOTIFICATION_WORKER_NAKE: &str = "event_notification_worker";
 
 #[tracing::instrument(skip(ctx), name = EVENT_NOTIFICATION_WORKER_NAKE)]
 pub async fn perform_event_notification(_job: Job, ctx: Data<WorkerState>) -> Result<(), Error> {
+    let latest_event = ctx
+        .db
+        .list_events(Some(ListEventFilter {
+            common: ListEventFilterCommon {
+                user_id: ctx.user_id.clone(),
+                limit: Some(1),
+            },
+            specific: ListEventFilterSpecific::DateRange {
+                start: Utc::now(),
+                end: Utc::now() + Duration::minutes(5),
+            },
+        }))
+        .await
+        .map_err(|e| crate::Error::Db(e).as_worker_error())?;
+
+    if let Some(event) = latest_event.first() {
+        hypr_notification2::show(hypr_notification2::Notification {
+            title: "Scheduled meeting in 5 minutes".to_string(),
+            message: event.name.clone(),
+            // TODO: This doesn't matter because we're hardcoding the destination in the deeplink handler
+            url: Some("hypr://todo".to_string()),
+            timeout: Some(std::time::Duration::from_secs(10)),
+        });
+    }
+
     Ok(())
 }
 

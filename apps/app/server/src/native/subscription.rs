d0f775322a4f8a480e71ddb488f179e972d90d57
yujonglee <yujonglee.dev@gmail.com>
2025-05-10 10:45:45 -0700

    update subscription endpoint

--- apps/app/server/src/native/subscription.rs
+++ apps/app/server/src/native/subscription.rs
@@ -1,16 +1,59 @@
-use axum::{extract::State, http::StatusCode, Extension, Json};
-
-use crate::state::AppState;
+use axum::{http::StatusCode, Extension, Json};
 
 pub async fn handler(
     Extension(billing): Extension<hypr_db_admin::Billing>,
 ) -> Result<Json<Subscription>, StatusCode> {
-    // let a = billing.stripe_subscription.map(|s| s.);
+    let subscription = billing.stripe_subscription.map(|s| Subscription {
+        status: s.status.into(),
+        current_period_end: s.current_period_end,
+        trial_end: s.trial_end,
+        plan: s
+            .items
+            .data
+            .first()
+            .map(|item| item.price.as_ref().map(|p| p.id.to_string()))
+            .flatten(),
+    });
 
-    let subscription = Subscription {};
-
-    Ok(Json(subscription))
+    if let Some(subscription) = subscription {
+        Ok(Json(subscription))
+    } else {
+        Err(StatusCode::NOT_FOUND)
+    }
 }
 
 #[derive(Debug, serde::Serialize, schemars::JsonSchema)]
-pub struct Subscription {}
+pub struct Subscription {
+    pub status: SubscriptionStatus,
+    pub current_period_end: i64,
+    pub trial_end: Option<i64>,
+    pub plan: Option<String>,
+}
+
+#[derive(Debug, serde::Serialize, schemars::JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum SubscriptionStatus {
+    Active,
+    Canceled,
+    Incomplete,
+    IncompleteExpired,
+    PastDue,
+    Paused,
+    Trialing,
+    Unpaid,
+}
+
+impl From<stripe::SubscriptionStatus> for SubscriptionStatus {
+    fn from(status: stripe::SubscriptionStatus) -> Self {
+        match status {
+            stripe::SubscriptionStatus::Active => SubscriptionStatus::Active,
+            stripe::SubscriptionStatus::Canceled => SubscriptionStatus::Canceled,
+            stripe::SubscriptionStatus::Incomplete => SubscriptionStatus::Incomplete,
+            stripe::SubscriptionStatus::IncompleteExpired => SubscriptionStatus::IncompleteExpired,
+            stripe::SubscriptionStatus::PastDue => SubscriptionStatus::PastDue,
+            stripe::SubscriptionStatus::Paused => SubscriptionStatus::Paused,
+            stripe::SubscriptionStatus::Trialing => SubscriptionStatus::Trialing,
+            stripe::SubscriptionStatus::Unpaid => SubscriptionStatus::Unpaid,
+        }
+    }
+}

f3141b6812a3395daffa15c4295b0ad78a4ffe96
yujonglee <yujonglee.dev@gmail.com>
2025-05-11 15:04:54 -0700

    wip

--- apps/desktop/src/routes/app.transcript.$id.tsx
+++ apps/desktop/src/routes/app.transcript.$id.tsx
@@ -1,21 +1,39 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 import { commands as dbCommands } from "@hypr/plugin-db";
+import TranscriptEditor from "@hypr/tiptap/transcript";
 
 export const Route = createFileRoute("/app/transcript/$id")({
   component: Component,
-  loader: async ({ params: { id } }) => {
-    const session = await dbCommands.getSession({ id });
-    return { session };
+  loader: async ({ params: { id }, context: { onboardingSessionId } }) => {
+    const timeline = onboardingSessionId
+      ? await dbCommands.getTimelineViewOnboarding()
+      : await dbCommands.getTimelineView(id);
+
+    return { timeline };
   },
 });
 
 function Component() {
-  const { session } = Route.useLoaderData();
+  const { timeline } = Route.useLoaderData();
+
+  const content = {
+    type: "doc",
+    content: [
+      {
+        type: "speaker",
+        attrs: { label: "" },
+        content: (timeline?.items || []).flatMap((item) => item.text.split(" ")).filter(Boolean).map((word) => ({
+          type: "word",
+          content: [{ type: "text", text: word }],
+        })),
+      },
+    ],
+  };
 
   return (
-    <div>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
+    <div className="p-12">
+      <TranscriptEditor initialContent={content} />
     </div>
   );
 }

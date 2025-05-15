4ad4f8697a034569a70597a7eca0fac90c67ecee
yujonglee <yujonglee.dev@gmail.com>
2025-05-15 11:29:34 -0700

    wip

--- crates/audio/src/mic.rs
+++ crates/audio/src/mic.rs
@@ -1,5 +1,30 @@
 pub use kalosm_sound::{MicInput, MicStream};
 
+#[cfg(target_os = "macos")]
+pub fn listener() {
+    use cidre::{core_audio as ca, os};
+
+    extern "C-unwind" fn callback(
+        _obj_id: ca::Obj,
+        _number_addresses: u32,
+        _addresses: *const ca::PropAddr,
+        client_data: *mut usize,
+    ) -> os::Status {
+        unsafe { client_data.write(1) };
+        os::Status::NO_ERR
+    }
+
+    let mut data = 0;
+
+    ca::System::OBJ
+        .add_prop_listener(
+            &ca::PropSelector::HW_DEFAULT_INPUT_DEVICE.global_addr(),
+            callback,
+            &mut data,
+        )
+        .unwrap();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;

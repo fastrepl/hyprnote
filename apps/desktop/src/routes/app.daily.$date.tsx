c86da6a5465dd89d8c1742b4de26a43bfcdad8e7
yujonglee <yujonglee.dev@gmail.com>
2025-05-14 21:42:04 -0700

    wip

--- apps/desktop/src/routes/app.daily.$date.tsx
+++ apps/desktop/src/routes/app.daily.$date.tsx
@@ -1,31 +1,39 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
-import { commands as dbCommands } from "@hypr/plugin-db";
 export const Route = createFileRoute("/app/daily/$date")({
-  beforeLoad: async ({ context: { queryClient, userId }, params: { date } }) => {
-    const existingSession = await queryClient.fetchQuery({
-      queryKey: ["daily", date],
-      // TODO
-      queryFn: () => dbCommands.getSession({ id: date }),
-    });
+  // TODO
 
-    if (existingSession) {
-      redirect({ to: "/app/note/$id", params: { id: existingSession.id } });
-    }
+  // beforeLoad: async ({ context: { queryClient, userId }, params: { date } }) => {
+  //   const existingSession = await queryClient.fetchQuery({
+  //     queryKey: ["daily", date],
+  //     // TODO
+  //     queryFn: () => dbCommands.getSession({ id: date }),
+  //   });
 
-    const newSession = await dbCommands.upsertSession({
-      id: date,
-      created_at: new Date().toISOString(),
-      visited_at: new Date().toISOString(),
-      user_id: userId,
-      calendar_event_id: null,
-      title: date,
-      raw_memo_html: "",
-      enhanced_memo_html: null,
-      conversations: [],
-      is_meeting: false,
-    });
+  //   if (existingSession) {
+  //     redirect({ to: "/app/note/$id", params: { id: existingSession.id } });
+  //   }
 
-    redirect({ to: "/app/note/$id", params: { id: newSession.id } });
-  },
+  //   const newSession = await dbCommands.upsertSession({
+  //     id: date,
+  //     created_at: new Date().toISOString(),
+  //     visited_at: new Date().toISOString(),
+  //     user_id: userId,
+  //     calendar_event_id: null,
+  //     title: date,
+  //     raw_memo_html: "",
+  //     enhanced_memo_html: null,
+  //     conversations: [],
+  //     is_meeting: false,
+  //   });
+
+  //   redirect({ to: "/app/note/$id", params: { id: newSession.id } });
+  // },
+
+  component: Component,
 });
+
+function Component() {
+  const { date } = Route.useParams();
+  return <div>{date}</div>;
+}

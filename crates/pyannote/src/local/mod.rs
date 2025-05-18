b1ea9e951ffb75c408ad7c0b888bbd0b034d49bf
yujonglee <yujonglee.dev@gmail.com>
2025-05-17 23:39:45 -0700

    add embedding

--- crates/pyannote/src/local/mod.rs
+++ crates/pyannote/src/local/mod.rs
@@ -1,4 +1,4 @@
-const EMBEDDING_ONNX: &[u8] = include_bytes!("./data/embedding.onnx");
-const SEGMENTATION_ONNX: &[u8] = include_bytes!("./data/segmentation.onnx");
+mod embedding;
+mod segmentation;
 
 pub fn run() {}
---
+++ crates/pyannote/src/local/segmentation.rs
@@ -0,0 +1,1 @@
+const SEGMENTATION_ONNX: &[u8] = include_bytes!("./data/segmentation.onnx");

1e43573723bde3ee33ffce7ecd5ec7d4d44aea56
henry <devgony@gmail.com>
2025-07-01 08:15:39 +0900

    set /MDd

--- build.rs
+++ build.rs
@@ -1,13 +1,23 @@
-fn main() {
-    // if env::var("TARGET").is_ok_and(|s| s.contains("windows-msvc")) {
-    //     // MSVC compiler suite
-    //     if env::var("CFLAGS").is_ok_and(|s| s.contains("/MDd")) {
-    //         // debug runtime flag is set
+use std::env;
 
-    //         // Don't link the default CRT
-    //         println!("cargo::rustc-link-arg=/nodefaultlib:msvcrt");
-    //         // Link the debug CRT instead
-    //         println!("cargo::rustc-link-arg=/defaultlib:msvcrtd");
-    //     }
-    // }
+fn main() {
+    // Force debug runtime for all dependencies on Windows MSVC
+    if env::var("TARGET").map_or(false, |s| s.contains("windows-msvc")) {
+        // Always use debug runtime in debug builds
+        if env::var("PROFILE").map_or(false, |p| p == "debug")
+            || env::var("CXXFLAGS").map_or(false, |s| s.contains("/MDd"))
+        {
+            // Don't link the default CRT
+            println!("cargo::rustc-link-arg=/nodefaultlib:msvcrt");
+            // Link the debug CRT instead
+            println!("cargo::rustc-link-arg=/defaultlib:msvcrtd");
+
+            // Set environment variables for C/C++ compilation
+            println!("cargo::rustc-env=CFLAGS=/MDd");
+            println!("cargo::rustc-env=CXXFLAGS=/MDd");
+
+            // Add additional debug runtime link arguments
+            println!("cargo::rustc-link-arg-bins=/defaultlib:msvcrtd");
+        }
+    }
 }

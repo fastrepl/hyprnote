e4d84ecdb1c8ac65bbc3c9da9ffc5064603561a1
henry <devgony@gmail.com>
2025-07-01 09:54:14 +0900

    set ORT_STRATEGY compile

--- build.rs
+++ build.rs
@@ -30,13 +30,22 @@ fn main() {
             println!("cargo::rustc-env=WHISPER_CFLAGS=/MDd");
             println!("cargo::rustc-env=WHISPER_CXXFLAGS=/MDd");
 
-            // For ONNX Runtime
+            // For ONNX Runtime - comprehensive settings
             println!("cargo::rustc-env=ORT_USE_CUDA=OFF");
             println!("cargo::rustc-env=ORT_BUILD_SHARED_LIB=OFF");
+            println!("cargo::rustc-env=ORT_STRATEGY=compile");
+            println!("cargo::rustc-env=ORT_DEBUG=1");
+            println!("cargo::rustc-env=ORT_PREFER_DYNAMIC_LINK=ON");
+            println!("cargo::rustc-env=ORT_CMAKE_EXTRA_ARGS=-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebugDLL -DCMAKE_CXX_FLAGS=/MDd -DCMAKE_C_FLAGS=/MDd");
+
+            // Force compilation from source
+            println!("cargo::rustc-env=ORT_LIB_LOCATION=");
+            println!("cargo::rustc-env=ORT_INCLUDE_DIR=");
 
             // Pass to dependencies
             println!("cargo::rerun-if-env-changed=CFLAGS");
             println!("cargo::rerun-if-env-changed=CXXFLAGS");
+            println!("cargo::rerun-if-env-changed=ORT_STRATEGY");
 
             // Additional debug runtime link arguments
             println!("cargo::rustc-link-arg-bins=/defaultlib:msvcrtd");

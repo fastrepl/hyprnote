b42fb40b92b796362a19766050e9123040517018
Yujong Lee <yujonglee.dev@gmail.com>
2025-07-08 21:48:10 -0700

    chores

--- apps/admin/src/routes/__root.tsx
+++ apps/admin/src/routes/__root.tsx
@@ -18,8 +18,8 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       ...seo({
-        title: "TanStack Start | Type-Safe, Client-First, Full-Stack React Framework",
-        description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
+        title: "Hyprnote Admin",
+        description: "Hyprnote Admin",
       }),
     ],
     links: [

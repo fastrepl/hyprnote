6424e788f600e758bbf94e42771a995b89c07750
plyght <plyght@peril.lol>
2025-07-14 18:04:25 +0200

    fix package json

--- plugins/meeting-automation/js/index.ts
+++ plugins/meeting-automation/js/index.ts
@@ -1,50 +1,17 @@
 export * from "./bindings.gen";
 
 export interface MeetingAutomationEvents {
-  recording_auto_started: MeetingDetectionEvent;
-  recording_auto_stopped: MeetingDetectionEvent;
+  recording_auto_started: {
+    app_name: string;
+    session_id: string;
+    timestamp: string;
+  };
+  recording_auto_stopped: {
+    timestamp: string;
+  };
   meeting_notification: {
     title: string;
     message: string;
-    actions: Array<[string, string]>;
+    timestamp: string;
   };
-}
-
-export interface MeetingDetectionEvent {
-  event_type: MeetingEventType;
-  app_bundle_id: string;
-  app_name: string;
-  timestamp: string;
-  metadata: Record<string, string>;
-}
-
-export enum MeetingEventType {
-  AppLaunched = "AppLaunched",
-  AppTerminated = "AppTerminated",
-  MicActivityDetected = "MicActivityDetected",
-  MicActivityStopped = "MicActivityStopped",
-  ScheduledMeetingStarting = "ScheduledMeetingStarting",
-  ScheduledMeetingEnding = "ScheduledMeetingEnding",
-  WindowFocused = "WindowFocused",
-  WindowUnfocused = "WindowUnfocused",
-}
-
-export interface AutomationConfig {
-  enabled: boolean;
-  auto_start_on_app_detection: boolean;
-  auto_start_on_mic_activity: boolean;
-  auto_stop_on_app_exit: boolean;
-  auto_start_scheduled_meetings: boolean;
-  require_window_focus: boolean;
-  pre_meeting_notification_minutes: number;
-  supported_apps: string[];
-  notification_settings: NotificationSettings;
-}
-
-export interface NotificationSettings {
-  show_meeting_started: boolean;
-  show_meeting_ending: boolean;
-  show_pre_meeting_reminder: boolean;
-  show_recording_started: boolean;
-  show_recording_stopped: boolean;
 }

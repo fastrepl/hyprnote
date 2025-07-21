5d3e4a4e1cd0890f0b301a2b757ae0d9c5911b1d
Deokhaeng Lee <l.deokhaeng@gmail.com>
2025-07-21 15:07:43 -0700

    ran tests

--- apps/desktop/src/components/settings/views/ai.tsx
+++ apps/desktop/src/components/settings/views/ai.tsx
@@ -492,7 +492,7 @@ export default function LocalAI() {
                   if (model.downloaded) {
                     setSelectedSTTModel(model.key);
                     localSttCommands.setCurrentModel(model.key as any);
-    
+
                     localSttCommands.restartServer();
                   }
                 }}
@@ -633,7 +633,7 @@ export default function LocalAI() {
                     setSelectedLLMModel(model.key);
                     localLlmCommands.setCurrentModel(model.key as SupportedModel);
                     setCustomLLMEnabledMutation.mutate(false);
-                    
+
                     localLlmCommands.restartServer();
                   }
                 }}

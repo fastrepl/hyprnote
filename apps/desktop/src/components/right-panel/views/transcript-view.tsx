cc3a24c97246f07b242e1440ea5468b77803b2d7
Yujong Lee <yujonglee.dev@gmail.com>
2025-09-06 13:54:49 -0700

    fix transcript editor persistance

--- apps/desktop/src/components/right-panel/views/transcript-view.tsx
+++ apps/desktop/src/components/right-panel/views/transcript-view.tsx
@@ -99,6 +99,7 @@ function RenderNotInMeeting({ sessionId, words }: { sessionId: string; words: Wo
   const [editable, setEditable] = useState(false);
   const [isSearchActive, setIsSearchActive] = useState(false);
   const speakerChunks = useMemo(() => wordsToSpeakerChunks(words), [words]);
+  const [editorWords, setEditorWords] = useState<Word2[]>([]);
 
   const editorRef = useRef<TranscriptEditorRef | null>(null);
   const { isAtBottom, scrollContainerRef, handleScroll, scrollToBottom } = useScrollToBottom([speakerChunks]);
@@ -115,6 +116,20 @@ function RenderNotInMeeting({ sessionId, words }: { sessionId: string; words: Wo
       }
     }
   }, [words]);
+
+  useEffect(() => {
+    if (!editable) {
+      dbCommands.getSession({ id: sessionId }).then((session) => {
+        if (session) {
+          dbCommands.upsertSession({ ...session, words: editorWords }).then(() => {
+            queryClient.invalidateQueries({
+              queryKey: ["session", "words", sessionId],
+            });
+          });
+        }
+      });
+    }
+  }, [editable]);
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
@@ -154,11 +169,7 @@ function RenderNotInMeeting({ sessionId, words }: { sessionId: string; words: Wo
   }, []);
 
   const handleUpdate = (words: Word2[]) => {
-    dbCommands.getSession({ id: sessionId }).then((session) => {
-      if (session) {
-        dbCommands.upsertSession({ ...session, words });
-      }
-    });
+    setEditorWords(words);
   };
 
   if (isSearchActive) {

88cfef01ea2ae120869a3e5ce610a7e869636208
Yujong Lee <yujonglee.dev@gmail.com>
2025-10-02 16:33:21 +0900

    wip

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,39 +1,36 @@
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
-// TODO: this is kind of isolated. need drizzle-zod bridge. we might want to just remove zod schemas from persisted.ts
+const SHARED = {
+  id: uuid("id").primaryKey().defaultRandom(),
+  // it is crucial to have user_id for sync filtering
+  user_id: uuid("user_id").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+};
 
 export const TABLE_HUMANS = "humans";
 export const humans = pgTable(TABLE_HUMANS, {
-  id: uuid("id").primaryKey().defaultRandom(),
+  ...SHARED,
   name: text("name").notNull(),
   email: text("email").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
 export const TABLE_ORGANIZATIONS = "organizations";
 export const organizations = pgTable(TABLE_ORGANIZATIONS, {
-  id: uuid("id").primaryKey().defaultRandom(),
+  ...SHARED,
   name: text("name").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
 export const TABLE_SESSIONS = "sessions";
 export const sessions = pgTable(TABLE_SESSIONS, {
-  id: uuid("id").primaryKey().defaultRandom(),
-  humanId: uuid("human_id").notNull().references(() => humans.id),
+  ...SHARED,
   title: text("title").notNull(),
   body: text("body").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
 });
 
 export const TABLE_EVENTS = "events";
 export const events = pgTable(TABLE_EVENTS, {
-  id: uuid("id").primaryKey().defaultRandom(),
-  humanId: uuid("human_id").notNull().references(() => humans.id),
+  ...SHARED,
   title: text("title").notNull(),
   startsAt: timestamp("starts_at").notNull(),
   endsAt: timestamp("ends_at").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
 });

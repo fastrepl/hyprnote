c7b5bf330e08d6ceff4f4a58eb9c078959d6f837
Yujong Lee <yujonglee.dev@gmail.com>
2025-10-10 19:32:53 +0900

    split files

---
+++ apps/desktop2/src/components/chat/index.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
+
+import * as persisted from "../../store/tinybase/persisted";
+import { id } from "../../utils";
+import { ChatBody } from "./body";
+import { ChatTrigger } from "./trigger";
+
+export function Chat() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useHotkeys("esc", () => setIsOpen(false));
+
+  const handleAddMessage = persisted.UI.useSetRowCallback(
+    "chat_messages",
+    id(),
+    (row: persisted.ChatMessage) => ({
+      ...row,
+      metadata: JSON.stringify(row.metadata),
+      parts: JSON.stringify(row.parts),
+    } satisfies persisted.ChatMessage),
+    [],
+    persisted.STORE_ID,
+  );
+
+  return (
+    <>
+      <ChatBody isOpen={isOpen} onClose={() => setIsOpen(false)} handleAddMessage={handleAddMessage} />
+      {!isOpen && <ChatTrigger onClick={() => setIsOpen(true)} />}
+    </>
+  );
+}

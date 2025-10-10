e3500831c5b627928c8b509a9a32659f55dd527b
Yujong Lee <yujonglee.dev@gmail.com>
2025-10-11 08:54:01 +0900

    split components

--- apps/desktop2/src/components/chat/index.tsx
+++ apps/desktop2/src/components/chat/index.tsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 
+import { cn } from "@hypr/ui/lib/utils";
+import { useAutoCloser } from "../../hooks/useAutoCloser";
 import * as persisted from "../../store/tinybase/persisted";
 import { id } from "../../utils";
+
 import { ChatBody } from "./body";
+import { ChatHeader } from "./header";
+import { ChatMessageInput } from "./input";
 import { ChatTrigger } from "./trigger";
 
 export function Chat() {
   const [isOpen, setIsOpen] = useState(false);
+  const chatRef = useAutoCloser(() => setIsOpen(false), isOpen);
 
   useHotkeys("meta+j", () => setIsOpen((prev) => !prev));
 
@@ -25,14 +31,25 @@ export function Chat() {
 
   return (
     <>
-      <ChatBody
-        // TODO
-        currentChatId={id()}
-        isOpen={isOpen}
-        handleClose={() => setIsOpen(false)}
-        handleAddMessage={handleAddMessage}
-      />
-      {!isOpen && <ChatTrigger onClick={() => setIsOpen(true)} />}
+      {isOpen
+        ? (
+          <div
+            ref={chatRef}
+            className={cn(
+              "fixed bottom-4 right-4 z-40",
+              "w-[440px] h-[600px] max-h-[calc(100vh-120px)]",
+              "bg-white rounded-2xl shadow-2xl",
+              "border border-neutral-200",
+              "flex flex-col",
+              "animate-in slide-in-from-bottom-4 fade-in duration-200",
+            )}
+          >
+            <ChatHeader currentChatId={id()} handleClose={() => setIsOpen(false)} />
+            <ChatBody />
+            <ChatMessageInput handleAddMessage={handleAddMessage} />
+          </div>
+        )
+        : <ChatTrigger onClick={() => setIsOpen(true)} />}
     </>
   );
 }

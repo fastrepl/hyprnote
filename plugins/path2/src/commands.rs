72c6fcdf1db0ae27fcafef2f6f79b6e01c87d39d
Yujong Lee <yujonglee.dev@gmail.com>
2026-01-01 23:33:36 +0900

    chores

--- plugins/path2/src/commands.rs
+++ plugins/path2/src/commands.rs
@@ -2,7 +2,7 @@ use crate::Path2PluginExt;
 
 #[tauri::command]
 #[specta::specta]
-pub(crate) async fn base<R: tauri::Runtime>(app: tauri::AppHandle<R>) -> Result<String, String> {
+pub(crate) fn base<R: tauri::Runtime>(app: tauri::AppHandle<R>) -> Result<String, String> {
     app.path2()
         .base()
         .map(|p| p.to_string_lossy().to_string())

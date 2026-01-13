5cd1ff5ad3c370505d3a951538059841fa5b2d65
ComputelessComputer <john@hyprnote.com>
2026-01-13 12:46:56 +0900

    refactor(admin): rename collections route to articles

--- apps/web/src/routes/admin/route.tsx
+++ apps/web/src/routes/admin/route.tsx
@@ -76,7 +76,7 @@ function AdminHeader({ user }: { user: { email: string } }) {
               className="relative py-1 text-sm text-neutral-600 hover:text-neutral-900 transition-colors [&.active]:text-neutral-900 font-medium [&.active]:after:absolute [&.active]:after:bottom-0 [&.active]:after:left-1/2 [&.active]:after:-translate-x-1/2 [&.active]:after:w-7 [&.active]:after:h-0.5 [&.active]:after:bg-neutral-900 [&.active]:after:rounded-full"
               activeProps={{ className: "active" }}
             >
-              Collections
+              Articles
             </Link>
             <Link
               to="/admin/media"

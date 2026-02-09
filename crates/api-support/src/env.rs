8794808f0df8300160eecb910b54e18155ea2c31
Yujong Lee <yujonglee.dev@gmail.com>
2026-02-09 10:47:52 +0900

    rename things

--- crates/api-support/src/env.rs
+++ crates/api-support/src/env.rs
@@ -2,9 +2,9 @@ use serde::Deserialize;
 
 #[derive(Clone, Deserialize)]
 pub struct GitHubAppEnv {
-    pub github_app_id: u64,
-    pub github_app_private_key: String,
-    pub github_app_installation_id: u64,
+    pub github_bot_app_id: u64,
+    pub github_bot_private_key: String,
+    pub github_bot_installation_id: u64,
     pub github_repo_id: String,
     pub github_discussion_category_id: String,
 }
